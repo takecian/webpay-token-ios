390f7466749381454999f706f030b2526a4f7420
yohei okada <yohei.states@akane.waseda.jp>
2014-03-05 15:35:06 -0800

    create Xcode project with empty application
    created a clean slate xcode project

---
+++ webpay-token-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  webpay-token-ios
+//
+//  Created by yohei on 3/5/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
