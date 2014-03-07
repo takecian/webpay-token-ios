7d44290a61c347b7ad8ac5fc45d1621715527bd1
yohei okada <yohei.states@akane.waseda.jp>
2014-03-07 14:21:23 -0800

    fix build error due to rename of test directory
    update path of files under test directory in xcode

---
+++ WebpayTests/TestWebpay.m
@@ -0,0 +1,34 @@
+//
+//  TestWebpay.m
+//  Webpay
+//
+//  Created by yohei on 3/7/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestWebpay : XCTestCase
+
+@end
+
+@implementation TestWebpay
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTAssertNil(nil, @"");
+}
+
+@end
