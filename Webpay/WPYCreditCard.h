9753edcae6e14622ad5a740605acc063642b6dfc
yohei okada <yohei.states@akane.waseda.jp>
2014-03-11 15:01:00 -0700

    create WPYCreditCard class file

---
+++ Webpay/WPYCreditCard.h
@@ -0,0 +1,13 @@
+//
+//  WPYCreditCard.h
+//  Webpay
+//
+//  Created by yohei on 3/11/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WPYCreditCard : NSObject
+
+@end
---
+++ Webpay/WPYCreditCard.m
@@ -0,0 +1,13 @@
+//
+//  WPYCreditCard.m
+//  Webpay
+//
+//  Created by yohei on 3/11/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import "WPYCreditCard.h"
+
+@implementation WPYCreditCard
+
+@end
