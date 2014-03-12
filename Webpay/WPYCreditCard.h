733cb40a5fbbb04776168178eaaf3238386ea902
yohei okada <yohei.states@akane.waseda.jp>
2014-03-12 14:40:45 -0700

    declare properies for WPYCreditCard
    copy for string, and assign for non-objects.

--- Webpay/WPYCreditCard.h
+++ Webpay/WPYCreditCard.h
@@ -8,6 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSUInteger, WPYMonth) {
+    WPYJanuary = 1,
+    WPYFebruary,
+    WPYMarch,
+    WPYApril,
+    WPYMay,
+    WPYJune,
+    WPYJuly,
+    WPYAugust,
+    WPYSeptember,
+    WPYOctober,
+    WPYNovember,
+    WPYDecember
+};
+
 @interface WPYCreditCard : NSObject
+
+@property(nonatomic, copy) NSString *name;
+@property(nonatomic, copy) NSString *number;
+@property(nonatomic, copy) NSString *cvc;
+@property(nonatomic, assign) WPYMonth expiryMonth;
+@property(nonatomic, assign) NSUInteger expiryYear;
 
 @end
