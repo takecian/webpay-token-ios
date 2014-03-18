adb7dd35cc768e5848e73bf22cc03200382f5013
yohei okada <yohei.states@akane.waseda.jp>
2014-03-18 13:50:04 -0700

    refactor out regex match into a method
    made a method that returns boolean if the passed
    string matches the regex passed.

--- Webpay/WPYCreditCard.m
+++ Webpay/WPYCreditCard.m
@@ -19,44 +19,45 @@
     }
     else
     {
-        NSRange visaRange = [cardNum rangeOfString:@"4[0-9]{12}(?:[0-9]{3})?" options:NSRegularExpressionSearch];
-        if (visaRange.location != NSNotFound)
+        if ([self isMatchWithRegex:@"4[0-9]{12}(?:[0-9]{3})?" string:cardNum])
         {
             return @"Visa";
         }
-        
-        NSRange amexRange = [cardNum rangeOfString:@"3[47][0-9]{13}" options:NSRegularExpressionSearch];
-        if (amexRange.location != NSNotFound)
+
+        if ([self isMatchWithRegex:@"3[47][0-9]{13}" string:cardNum])
         {
             return @"American Express";
         }
         
-        NSRange masterRange = [cardNum rangeOfString:@"5[1-5][0-9]{14}" options:NSRegularExpressionSearch];
-        if (masterRange.location != NSNotFound)
+        if ([self isMatchWithRegex:@"5[1-5][0-9]{14}" string:cardNum])
         {
             return @"MasterCard";
         }
-        
-        NSRange discoverRange = [cardNum rangeOfString:@"6(?:011|5[0-9]{2})[0-9]{12}" options:NSRegularExpressionSearch];
-        if (discoverRange.location != NSNotFound)
+
+        if ([self isMatchWithRegex:@"6(?:011|5[0-9]{2})[0-9]{12}" string:cardNum])
         {
             return @"Discover";
         }
-        
-        NSRange JCBRange = [cardNum rangeOfString:@"(?:2131|1800|35\\d{3})\\d{11}" options:NSRegularExpressionSearch];
-        if (JCBRange.location != NSNotFound)
+
+        if ([self isMatchWithRegex:@"(?:2131|1800|35\\d{3})\\d{11}" string:cardNum])
         {
             return @"JCB";
         }
         
-        NSRange dinersRange = [cardNum rangeOfString:@"3(?:0[0-5]|[68][0-9])[0-9]{11}" options:NSRegularExpressionSearch];
-        if (dinersRange.location != NSNotFound)
+        if ([self isMatchWithRegex:@"3(?:0[0-5]|[68][0-9])[0-9]{11}" string:cardNum])
         {
             return @"Diners";
         }
         
         return @"Unknown";
     }
+}
+
+#pragma mark private methods
+- (BOOL)isMatchWithRegex:(NSString *)regex string:(NSString *)string
+{
+    NSRange range = [string rangeOfString:regex options:NSRegularExpressionSearch];
+    return range.location != NSNotFound;
 }
 
 
