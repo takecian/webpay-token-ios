a04fa5f2ad1cd6f972fce7da0c1bbb49b6c08505
yohei okada <yohei.states@akane.waseda.jp>
2014-03-30 14:46:03 -0700

    create tokenizer class file and test file

---
+++ Webpay/WPYTokenizer.h
@@ -0,0 +1,13 @@
+//
+//  WPYTokenizer.h
+//  Webpay
+//
+//  Created by yohei on 3/30/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WPYTokenizer : NSObject
+
+@end
---
+++ Webpay/WPYTokenizer.m
@@ -0,0 +1,13 @@
+//
+//  WPYTokenizer.m
+//  Webpay
+//
+//  Created by yohei on 3/30/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import "WPYTokenizer.h"
+
+@implementation WPYTokenizer
+
+@end
