f6e2b4d9b6a09a384e6b73fb2cef93076fdee19f
yohei okada <yohei.states@akane.waseda.jp>
2014-04-05 14:54:01 -0700

    create WPYToken file and its test class

---
+++ Webpay/WPYToken.m
@@ -0,0 +1,13 @@
+//
+//  WPYToken.m
+//  Webpay
+//
+//  Created by yohei on 4/5/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import "WPYToken.h"
+
+@implementation WPYToken
+
+@end
---
+++ WebpayTests/WPYTokenTest.m
@@ -0,0 +1,34 @@
+//
+//  WPYTokenTest.m
+//  Webpay
+//
+//  Created by yohei on 4/5/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WPYTokenTest : XCTestCase
+
+@end
+
+@implementation WPYTokenTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
