edb3ed5211310a10c305ef5bd02b1ff5798301e0
yohei okada <yohei.states@akane.waseda.jp>
2014-04-05 14:51:09 -0700

    create WPYTokenBuilder and its test class

---
+++ Webpay/WPYTokenBuilder.h
@@ -0,0 +1,13 @@
+//
+//  WPYTokenBuilder.h
+//  Webpay
+//
+//  Created by yohei on 4/5/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WPYTokenBuilder : NSObject
+
+@end
---
+++ Webpay/WPYTokenBuilder.m
@@ -0,0 +1,13 @@
+//
+//  WPYTokenBuilder.m
+//  Webpay
+//
+//  Created by yohei on 4/5/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import "WPYTokenBuilder.h"
+
+@implementation WPYTokenBuilder
+
+@end
