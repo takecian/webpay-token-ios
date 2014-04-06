54afef0dbee67184f58e6d64c9f82a7c7cd174c8
yohei okada <yohei.states@akane.waseda.jp>
2014-04-05 22:52:03 -0700

    create WPYErrorBuilder class and its test classes

---
+++ Webpay/WPYErrorBuilder.m
@@ -0,0 +1,17 @@
+//
+//  WPYErrorBuilder.m
+//  Webpay
+//
+//  Created by yohei on 4/5/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import "WPYErrorBuilder.h"
+
+@implementation WPYErrorBuilder
+- (NSError *)buildErrorFromData:(NSData *)data
+{
+    return nil;
+}
+
+@end
---
+++ WebpayTests/WPYErrorBuilderTest.m
@@ -0,0 +1,33 @@
+//
+//  WPYErrorBuilderTest.m
+//  Webpay
+//
+//  Created by yohei on 4/5/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WPYErrorBuilderTest : XCTestCase
+
+@end
+
+@implementation WPYErrorBuilderTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+}
+
+@end
