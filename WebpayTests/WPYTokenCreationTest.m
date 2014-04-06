db9a64bfb8a14f5a8931ad1f26e751d622405474
yohei okada <yohei.states@akane.waseda.jp>
2014-04-06 15:21:56 -0700

    rename WPYTokenizerTest to WPYTokenCreationTest
    Since this test will be an integration test, the 
    file name should not be ClassTest

---
+++ WebpayTests/WPYTokenCreationTest.m
@@ -0,0 +1,33 @@
+//
+//  WPYTokenCreationTest.m
+//  Webpay
+//
+//  Created by yohei on 4/6/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+// Integration test of WPYTokenizer
+
+#import <XCTest/XCTest.h>
+
+#import "WPYTokenizer.h"
+#import "WPYCreditCard.h"
+#import "WPYToken.h"
+
+@interface WPYTokenCreationTest : XCTestCase
+
+@end
+
+@implementation WPYTokenCreationTest
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+@end
