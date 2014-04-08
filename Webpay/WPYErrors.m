c3350b296d4b18dec066e52f55564e864efe2a12
yohei okada <yohei.states@akane.waseda.jp>
2014-04-07 18:00:32 -0700

    add an empty line between cases

--- Webpay/WPYErrors.m
+++ Webpay/WPYErrors.m
@@ -21,30 +21,43 @@ NSString * WPYLocalizedDescriptionFromErrorCode(WPYErrorCode errorCode)
     {
         case WPYInvalidNumber:
             return NSLocalizedStringFromTable(@"Card error: invalid number.", WPYLocalizedStringTable, nil);
+            
         case WPYIncorrectNumber:
             return NSLocalizedStringFromTable(@"", WPYLocalizedStringTable, nil);
+            
         case WPYInvalidName:
             return NSLocalizedStringFromTable(@"Card error: invalid name.", WPYLocalizedStringTable, nil);
+            
         case WPYInvalidExpiryMonth:
             return NSLocalizedStringFromTable(@"Card error: invalid expiry month.", WPYLocalizedStringTable, nil);
+            
         case WPYInvalidExpiryYear:
             return NSLocalizedStringFromTable(@"Card error: invalid expiry year.", WPYLocalizedStringTable, nil);
+            
         case WPYInvalidExpiry:
             return NSLocalizedStringFromTable(@"Card error: invalid expiry.", WPYLocalizedStringTable, nil);
+            
         case WPYIncorrectExpiry:
             return NSLocalizedStringFromTable(@"", WPYLocalizedStringTable, nil);
+            
         case WPYInvalidCvc:
             return NSLocalizedStringFromTable(@"Card error: invalid cvc.", WPYLocalizedStringTable, nil);
+            
         case WPYIncorrectCvc:
             return NSLocalizedStringFromTable(@"", WPYLocalizedStringTable, nil);
+            
         case WPYCardDeclined:
             return NSLocalizedStringFromTable(@"", WPYLocalizedStringTable, nil);
+            
         case WPYMissing:
             return NSLocalizedStringFromTable(@"", WPYLocalizedStringTable, nil);
+            
         case WPYProcessingError:
             return NSLocalizedStringFromTable(@"", WPYLocalizedStringTable, nil);
+            
         case WPYInvalidRequestError:
             return NSLocalizedStringFromTable(@"", WPYLocalizedStringTable, nil);
+            
         case WPYAPIError:
             return NSLocalizedStringFromTable(@"", WPYLocalizedStringTable, nil);
             
