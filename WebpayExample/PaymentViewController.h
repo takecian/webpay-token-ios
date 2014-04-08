c7077871777614441950bf61079f7f90757601d2
yohei okada <yohei.states@akane.waseda.jp>
2014-04-08 15:45:25 -0700

    create PaymentViewController for example

---
+++ WebpayExample/PaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaymentViewController.h
+//  Webpay
+//
+//  Created by yohei on 4/8/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaymentViewController : UIViewController
+
+@end
