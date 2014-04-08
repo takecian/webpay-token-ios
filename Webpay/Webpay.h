00f4a19c014b0414c57892956d38d75d757eed52
yohei okada <yohei.states@akane.waseda.jp>
2014-04-08 16:36:06 -0700

    create framework header

---
+++ Webpay/Webpay.h
@@ -0,0 +1,18 @@
+//
+//  Webpay.h
+//  Webpay
+//
+//  Created by yohei on 4/8/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef _Webpay_
+    #define _Webpay_
+
+    #import "WPYTokenizer.h"
+    #import "WPYCreditCard.h"
+    #import "WPYToken.h"
+    #import "WPYErrors.h"
+#endif
