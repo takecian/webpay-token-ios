4a5ba3fa7db25fbe6aa9e76757ade4d05a0b5dad
yohei okada <yohei.states@akane.waseda.jp>
2014-04-11 23:27:45 -0700

    create WPYPaymentController

---
+++ Webpay/WPYPaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  WPYPaymentViewController.h
+//  Webpay
+//
+//  Created by yohei on 4/11/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPYPaymentViewController : UIViewController
+
+@end
