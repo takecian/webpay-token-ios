c8d7ddba74f8111449a79094a29138324436847e
yohei okada <yohei.states@akane.waseda.jp>
2014-04-16 00:30:23 -0700

    create WPYNameField

---
+++ Webpay/CardForm/WPYNameField.h
@@ -0,0 +1,13 @@
+//
+//  WPYNameField.h
+//  Webpay
+//
+//  Created by yohei on 4/16/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPYNameField : UITextField
+
+@end
