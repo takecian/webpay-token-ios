b3c11dd6bceb1998a843a8a50990aeece38a92fc
yohei okada <yohei.states@akane.waseda.jp>
2014-04-16 00:32:09 -0700

    add keyboard initialization

--- Webpay/CardForm/WPYNameField.m
+++ Webpay/CardForm/WPYNameField.m
@@ -13,19 +13,12 @@
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
+    if (self)
+    {
+        self.placeholder = @"Taro Yamada";
+        self.keyboardType = UIKeyboardTypeASCIICapable;
     }
     return self;
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
