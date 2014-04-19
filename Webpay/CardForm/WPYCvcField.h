29988e09097a2e06fe9dfd798b171f013646382f
yohei okada <yohei.states@akane.waseda.jp>
2014-04-18 23:17:09 -0700

    subclass from abstractcardfield and refactor

--- Webpay/CardForm/WPYCvcField.h
+++ Webpay/CardForm/WPYCvcField.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface WPYCvcField : UIView
+#import "WPYAbstractCardFieldSubclass.h"
 
-- (NSString *)text;
+@interface WPYCvcField : WPYAbstractCardField
 @end
--- Webpay/CardForm/WPYCvcField.m
+++ Webpay/CardForm/WPYCvcField.m
@@ -8,43 +8,66 @@
 
 #import "WPYCvcField.h"
 
+#import "WPYCreditCard.h"
+
 @interface WPYCvcField () <UITextFieldDelegate>
 @end
 
-@implementation WPYCvcField
-{
-    UITextField *_cvcField;
-}
-
 static NSInteger const WPYCvcMaxValue = 4;
 
+@implementation WPYCvcField
 #pragma mark initialization
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self)
     {
-        _cvcField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
-        _cvcField.placeholder = @"123";
-        _cvcField.secureTextEntry = YES;
-        _cvcField.keyboardType = UIKeyboardTypeNumberPad;
-        _cvcField.delegate = self;
+        _textField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
+        _textField.placeholder = @"123";
+        _textField.secureTextEntry = YES;
+        _textField.keyboardType = UIKeyboardTypeNumberPad;
+        _textField.delegate = self;
         
-        [self addSubview:_cvcField];
+        [self addSubview:_textField];
     }
     return self;
 }
 
-- (NSString *)text
+
+
+#pragma mark
+- (WPYFieldKey)key
 {
-    return _cvcField.text;
+    return WPYCvcFieldKey;
 }
+
+
 
 #pragma mark textfield delegate
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)replacementString
 {
     NSString *newValue = [textField.text stringByReplacingCharactersInRange:range withString:replacementString];
     return newValue.length <= WPYCvcMaxValue;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    NSString *cvc = textField.text;
+    if (cvc.length == 0) // don't validate if cvc is not filled
+    {
+        return;
+    }
+    
+    WPYCreditCard *creditCard = [[WPYCreditCard alloc] init];
+    NSError *error = nil;
+    if ([creditCard validateCvc:&cvc error:&error])
+    {
+        [self notifySuccess];
+    }
+    else
+    {
+        [self notifyError:error];
+    }
 }
 
 @end
