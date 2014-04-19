257a8979ab433fa0aa92bd2766600def7974f3f2
yohei okada <yohei.states@akane.waseda.jp>
2014-04-18 23:48:12 -0700

    create button and its action

--- Webpay/WPYPaymentViewController.m
+++ Webpay/WPYPaymentViewController.m
@@ -8,42 +8,126 @@
 
 #import "WPYPaymentViewController.h"
 
-@interface WPYPaymentViewController ()
+#import "WPYCardFormView.h"
+#import "WPYTokenizer.h"
 
+@interface WPYPaymentViewController ()<WPYCardFormViewDelegate>
+@property(nonatomic, strong) WPYCreditCard *card;
+@property(nonatomic, strong) WPYCardFormView *cardForm;
+
+@property(nonatomic, copy) NSString *buttonTitle;
+@property(nonatomic, copy) WPYPaymentViewCallback callback;
+
+@property(nonatomic, strong) UIButton *payButton;
+@property(nonatomic, strong) UIActivityIndicatorView *indicator;
 @end
 
-@implementation WPYPaymentViewController
 
+
+static float const WPYCardFormViewHeight = 300.0f; // for covering up non keyboard & expiry picker area.
+
+
+@implementation WPYPaymentViewController
+#pragma mark initializer
+- (instancetype)initWithButtonTitle:(NSString *)title
+                           callback:(WPYPaymentViewCallback)callback
+{
+    if (self = [super initWithNibName:nil bundle:nil])
+    {
+        _callback = callback;
+        _buttonTitle = title ? title : NSLocalizedString(@"Confirm Payment", nil);
+    }
+    
+    return self;
+}
+
+// override designated initializer of super
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+    return [self initWithButtonTitle:nil callback:nil];
+}
+
+
+
+#pragma mark view lifecycle
+- (void)loadView
+{
+	[super loadView];
+	UIView *theView = [[UIView alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+	theView.backgroundColor = [UIColor whiteColor];
+	self.view = theView;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.cardForm = [[WPYCardFormView alloc] initWithFrame:CGRectMake(0, 0, 320, WPYCardFormViewHeight)];
+    self.cardForm.delegate = self;
+    [self.view addSubview: self.cardForm];
+    
+    
+    self.payButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.payButton.frame = CGRectMake(40, 320, 240, 44);
+    self.payButton.layer.cornerRadius = 2;
+    self.payButton.backgroundColor = [UIColor colorWithRed:0 green:0.478 blue:1.0 alpha:1.0];
+    [self.payButton setTitle:self.buttonTitle forState:UIControlStateNormal];
+    [self.payButton setTitle:@" " forState:UIControlStateSelected];
+    [self.payButton addTarget:self
+                       action:@selector(payButtonPushed:)
+             forControlEvents:UIControlEventTouchUpInside];
+    
+    self.indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    [self.indicator setCenter:CGPointMake(120, 22)];
+    [self.payButton addSubview:self.indicator];
+    
+    [self.view addSubview:self.payButton];
 }
 
+
+#pragma mark pay button
+- (void)payButtonPushed:(id)sender
+{
+    [self startIndicator];
+    WPYTokenizerCompletionBlock compBlock = ^(WPYToken *token, NSError *error)
+    {
+        [self stopIndicator];
+        self.callback(token, error);
+    };
+    [WPYTokenizer createTokenFromCard:self.card completionBlock: compBlock];
+}
+
+- (void)startIndicator
+{
+    [self.payButton setSelected:YES];
+    [self.indicator startAnimating];
+}
+
+- (void)stopIndicator
+{
+    [self.payButton setSelected:NO];
+    [self.indicator stopAnimating];
+}
+
+
+#pragma mark WPYCardFormDelegate
+- (void)invalidFieldName:(NSString *)fieldName error:(NSError *)error
+{
+}
+
+- (void)validFormWithCard:(WPYCreditCard *)creditCard
+{
+    self.card = creditCard;
+}
+
+
+
+#pragma mark memory management
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
