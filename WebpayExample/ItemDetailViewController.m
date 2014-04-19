58d8dd401801a294925ab0dbc576692c6c7d8fa7
yohei okada <yohei.states@akane.waseda.jp>
2014-04-18 20:49:36 -0700

    create button and its action when clicked

--- WebpayExample/ItemDetailViewController.m
+++ WebpayExample/ItemDetailViewController.m
@@ -8,6 +8,9 @@
 
 #import "ItemDetailViewController.h"
 
+#import "WPYPaymentViewController.h"
+#import "WPYToken.h"
+
 @interface ItemDetailViewController ()
 
 @end
@@ -26,6 +29,46 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UIButton *payButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    payButton.frame = CGRectMake(40, 280, 240, 44);
+    payButton.layer.cornerRadius = 2;
+    payButton.backgroundColor = [UIColor colorWithRed:0.18 green:0.8 blue:0.44 alpha:1];
+    [payButton setTitle:@"Pay with Card" forState:UIControlStateNormal];
+    [payButton addTarget:self action:@selector(showPaymentView:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:payButton];
+}
+
+
+#pragma mark event handler
+- (void)showPaymentView:(id)sender
+{
+    NSString *buttonTitle = @"Confirm and pay $1.00";
+    WPYPaymentViewCallback callback = ^(WPYToken *token, NSError *error)
+    {
+        if (token)
+        {
+            [[[UIAlertView alloc] initWithTitle:@"success!"
+                                        message:[NSString stringWithFormat:@"token:%@", token.tokenId]
+                                       delegate:nil
+                              cancelButtonTitle:@"ok"
+                              otherButtonTitles:nil, nil]
+             show];
+        }
+        else
+        {
+            [[[UIAlertView alloc] initWithTitle:@"error"
+                                        message:[error localizedDescription]
+                                       delegate:nil
+                              cancelButtonTitle:@"ok"
+                              otherButtonTitles:nil, nil]
+             show];
+        }
+    };
+    
+    WPYPaymentViewController *paymentViewController = [[WPYPaymentViewController alloc] initWithButtonTitle:buttonTitle callback:callback];
+    
+    [self.navigationController pushViewController:paymentViewController animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
