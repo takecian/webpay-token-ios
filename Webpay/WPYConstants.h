09a6e35fbb6214e5688a15789b152c35c1d16ed3
yohei okada <yohei.states@akane.waseda.jp>
2014-04-23 16:10:41 -0700

    create WPYConstants file

---
+++ Webpay/WPYConstants.h
@@ -0,0 +1,13 @@
+//
+//  WPYConstants.h
+//  Webpay
+//
+//  Created by yohei on 4/23/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WPYConstants : NSObject
+
+@end
---
+++ Webpay/WPYConstants.m
@@ -0,0 +1,13 @@
+//
+//  WPYConstants.m
+//  Webpay
+//
+//  Created by yohei on 4/23/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import "WPYConstants.h"
+
+@implementation WPYConstants
+
+@end
