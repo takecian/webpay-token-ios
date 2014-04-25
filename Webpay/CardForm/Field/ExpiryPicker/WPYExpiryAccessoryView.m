b58718fee8af58b0c1cfdf69a7600dd5a7b8b1cd
yohei okada <yohei.states@akane.waseda.jp>
2014-04-25 13:38:54 -0700

    create accessory view on top of expirypicker

---
+++ Webpay/CardForm/Field/ExpiryPicker/WPYExpiryAccessoryView.m
@@ -0,0 +1,46 @@
+//
+//  WPYExpiryAccessoryView.m
+//  Webpay
+//
+//  Created by yohei on 4/25/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import "WPYExpiryAccessoryView.h"
+
+#import "WPYConstants.h"
+
+@interface WPYExpiryAccessoryView ()
+@end
+
+@implementation WPYExpiryAccessoryView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        self.backgroundColor = [UIColor whiteColor];
+        self.layer.borderWidth = 0.5f;
+        self.layer.borderColor = [[UIColor colorWithRed:0.76 green:0.76 blue:0.76 alpha:1] CGColor];
+        
+        UIButton *doneButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        doneButton.frame = CGRectMake(250, 0, 70, 44);
+        [doneButton setTitle:NSLocalizedStringFromTable(@"Done", WPYLocalizedStringTable, nil)
+                     forState:UIControlStateNormal];
+        [doneButton addTarget:self action:@selector(doneTapped:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [self addSubview:doneButton];
+    }
+    return self;
+}
+
+- (void)doneTapped:(id)sender
+{
+    if (self.delegate && [self.delegate respondsToSelector:@selector(doneButtonTapped)])
+    {
+        [self.delegate doneButtonTapped];
+    }
+}
+
+@end
