8e9a71691d8942b232b512273acba8114798fc3c
yohei okada <yohei.states@akane.waseda.jp>
2014-05-04 16:17:27 -0700

    create WPYTextField

---
+++ Webpay/CardForm/Field/WPYTextField.m
@@ -0,0 +1,28 @@
+//
+//  WPYTextField.m
+//  Webpay
+//
+//  Created by yohei on 5/4/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import "WPYTextField.h"
+
+@implementation WPYTextField
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+    }
+    return self;
+}
+
+- (CGRect)rightViewRectForBounds:(CGRect)bounds
+{
+    CGRect textRect = [super rightViewRectForBounds:bounds];
+    return textRect;
+}
+
+@end
