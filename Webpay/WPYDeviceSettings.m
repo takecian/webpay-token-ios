2c1f4122a7628c8aaf9d6d87f01592717b43bd4c
yohei okada <yohei.states@akane.waseda.jp>
2014-05-15 14:16:24 -0700

    create WPYDeviceSettings file

---
+++ Webpay/WPYDeviceSettings.m
@@ -0,0 +1,13 @@
+//
+//  WPYDeviceSettings.m
+//  Webpay
+//
+//  Created by yohei on 5/15/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import "WPYDeviceSettings.h"
+
+@implementation WPYDeviceSettings
+
+@end
