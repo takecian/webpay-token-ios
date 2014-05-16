ee8dda583acc9789d88a967bdb46d46f6f3a3e9a
yohei okada <yohei.states@akane.waseda.jp>
2014-05-15 21:28:07 -0700

    create WPYBundleManager

---
+++ Webpay/WPYBundleManager.h
@@ -0,0 +1,13 @@
+//
+//  WPYBundleManager.h
+//  Webpay
+//
+//  Created by yohei on 5/15/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WPYBundleManager : NSObject
+
+@end
---
+++ Webpay/WPYBundleManager.m
@@ -0,0 +1,13 @@
+//
+//  WPYBundleManager.m
+//  Webpay
+//
+//  Created by yohei on 5/15/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import "WPYBundleManager.h"
+
+@implementation WPYBundleManager
+
+@end
