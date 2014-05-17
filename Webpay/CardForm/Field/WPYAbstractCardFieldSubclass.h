33c5c7103270b8de357b238cfef6627b7dd14cff
yohei okada <yohei.states@akane.waseda.jp>
2014-05-17 14:00:29 -0700

    create WPYAbstractCardFieldSubclass

---
+++ Webpay/CardForm/Field/WPYAbstractCardFieldSubclass.h
@@ -0,0 +1,14 @@
+//
+//  WPYAbstractCardFieldSubclass.h
+//  Webpay
+//
+//  Created by yohei on 5/17/14.
+//  Copyright (c) 2014 yohei, YasuLab. All rights reserved.
+//
+
+#import "WPYAbstractCardField.h"
+
+@interface WPYAbstractCardField ()
+// protected setter
+- (void)updateText:(NSString *)text;
+@end
