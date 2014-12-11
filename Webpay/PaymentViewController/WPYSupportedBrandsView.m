6e99af50ca4865831ece448e8f71a6352e24fe52
yohei okada <yohei.states@akane.waseda.jp>
2014-12-10 23:45:31 -0800

    Create brand logo views in WPYSupportedBrandsView
    - Storyboard didn't work well with custom bundle

--- Webpay/PaymentViewController/WPYSupportedBrandsView.m
+++ Webpay/PaymentViewController/WPYSupportedBrandsView.m
@@ -11,21 +11,19 @@
 #import "WPYBundleManager.h"
 
 @interface WPYSupportedBrandsView ()
-@property(nonatomic, weak) IBOutlet UIImageView *visaImage;
-@property(nonatomic, weak) IBOutlet UIImageView *masterCardImage;
-@property(nonatomic, weak) IBOutlet UIImageView *amexImage;
-@property(nonatomic, weak) IBOutlet UIImageView *jcbImage;
-@property(nonatomic, weak) IBOutlet UIImageView *dinersImage;
 @end
 
 @implementation WPYSupportedBrandsView
 
 - (void)showBrands:(NSArray *)brands
 {
-    self.visaImage.hidden = ![brands containsObject:@"Visa"];
-    self.masterCardImage.hidden = ![brands containsObject:@"MasterCard"];
-    self.amexImage.hidden = ![brands containsObject:@"American Express"];
-    self.jcbImage.hidden = ![brands containsObject:@"JCB"];
-    self.dinersImage.hidden = ![brands containsObject:@"Diners Club"];
+    [brands enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        NSString *brand = (NSString *)obj;
+        NSString *imageFileName = [NSString stringWithFormat:@"Small%@", [brand stringByReplacingOccurrencesOfString:@" " withString:@""]];
+        UIImage *brandImage = [WPYBundleManager imageNamed:imageFileName];
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(10 + (brandImage.size.width + 10) * idx + 5, 0, brandImage.size.width, brandImage.size.height)];
+        imageView.image = brandImage;
+        [self addSubview:imageView];
+    }];
 }
 @end
